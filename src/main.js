7f518dfd58b537856846483ee22f30901b4049ef
Michael Fivis <michael@mbp.local>
2020-01-05 13:21:00 +0100

    success

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 var config = {
     type: Phaser.CANVAS,
-    width: 400,
+    width: 800,
     height: 490,
     physics: {
         default: 'arcade',
@@ -31,21 +31,28 @@ function preload ()
 }
 
 // Play Bindings
-var player_one;
 var walls;
 var score = 0;
 var scoreText;
-var cursors;
 var flap_tween;
 function create ()
 {
     cursors = this.input.keyboard.createCursorKeys();
+    p2_key = this.input.keyboard.addKey('w');  // Get key object
 
-    this.add.image(200, 300, 'sky');
+    // Background
+    this.add.image(200, 300, 'sky'); // todo scale up instead
+    this.add.image(400, 300, 'sky');
+
+
     player_one = this.physics.add.sprite(100, 245, 'bird');
     player_one.body.setGravityY(1000);
     player_one.setCollideWorldBounds(true);
 
+    player_two = this.physics.add.sprite(500, 245, 'bird');
+    player_two.body.setGravityY(1000);
+    player_two.setCollideWorldBounds(true);
+    
     walls = this.physics.add.group();
 
     var timer = this.time.addEvent({
@@ -58,6 +65,7 @@ function create ()
 
     scoreText = this.add.text(16, 16, '0', { fontSize: '32px', fill: '#FFF' });
     this.physics.add.collider(player_one, walls, restarter, null, this);
+    this.physics.add.collider(player_two, walls, restarter, null, this);
 
     flap_tween = game.scene.keys['main'].tweens.add({
         targets: player_one,
@@ -85,16 +93,16 @@ function genWall() {
     var hole = Math.floor(Math.random() * 5) + 1;
 
     // Add the 6 pipes with one two-part hole at position 'hole' and 'hole + 1'
-    for (var i = 0; i < 8; i++)
-        if (i != hole && i != hole + 1) 
-            brickGen(400, i * 60 + 35);  
+    for (var i = 0; i < 9; i++)
+        if (i != hole && i != hole + 1 && i != hole + 2) 
+            brickGen(800, i * 60 + 35);  
     
     score += 1;
     scoreText.text = score;
 }
 
-function jump(player) {
-    player_one.body.velocity.y = -350;
+function jump(p) {
+    p.body.velocity.y = -350;
     this.flap_tween.play();
 }
 
@@ -105,8 +113,11 @@ function update () {
     }
 
     if (cursors.up.isDown)
-        jump(this.player_one);
+        jump(player_two);
 
+    if (p2_key.isDown)
+        jump(player_one);
+    
     if (player_one.angle < 20) {
         player_one.angle += 1;
     }
