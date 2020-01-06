3e7157b46fc479075cd799f6c85c09b730c73d96
Michael Fivis <mfivis@cisco.com>
2020-01-06 14:37:58 +0100

    touch zones added

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 var config = {
     type: Phaser.CANVAS,
-    width: 800,
-    height: 490,
+    width: 1200,
+    height: 735,
     physics: {
         default: 'arcade',
         arcade: {
@@ -24,7 +24,7 @@ function preload ()
     this.load.image('bird', 'img/bird.png');
     this.load.image('pipe', 'img/pipe.png');
     // Feeding in spritesheets
-    // this.load.spritesheet('dude', 
+    // this.load.spritesheet('dude',
     //     'img/dude.png',
     //     { frameWidth: 32, frameHeight: 48 }
     // );
@@ -41,22 +41,22 @@ function create ()
     p2_key = this.input.keyboard.addKey('w');  // Get key object
 
     // Background
-    this.add.image(200, 300, 'sky'); // todo scale up instead
-    this.add.image(400, 300, 'sky');
+    sky = this.add.image(200, 300, 'sky'); // todo scale up instead
+    sky.setScale(3);
 
-
-    player_one = this.physics.add.sprite(100, 245, 'bird');
+    // Daring
+    player_one = this.physics.add.sprite(250, 245, 'bird');
     player_one.body.setGravityY(1000);
     player_one.setCollideWorldBounds(true);
 
-    player_two = this.physics.add.sprite(500, 245, 'bird');
+    player_two = this.physics.add.sprite(750, 245, 'bird');
     player_two.body.setGravityY(1000);
     player_two.setCollideWorldBounds(true);
-    
+
     walls = this.physics.add.group();
 
     var timer = this.time.addEvent({
-        delay: 1500,                
+        delay: 1500,
         callback: genWall,
         //args: [],
         callbackScope: this,
@@ -68,16 +68,30 @@ function create ()
     this.physics.add.collider(player_two, walls, restarter, null, this);
 
     flap_tween = game.scene.keys['main'].tweens.add({
-        targets: player_one,
+        targets: player_one, player_two,
         angle: -20,
         ease: 'Linear',       // 'Cubic', 'Elastic', 'Bounce', 'Back'
         duration: 100,
         repeat: 0,            // -1: infinity
     });
+
+    this.input.addPointer();
+    var zone1 = this.add.zone(0, 0, 600, 735).setOrigin(0).setName('p1touch').setInteractive();
+    var zone2 = this.add.zone(600, 0, 600, 735).setOrigin(0).setName('p2touch').setInteractive();
+
+    this.input.on('gameobjectdown', function (pointer, gameObject) {
+        if (pointer.x < 600) {
+            jump(player_one);
+        }
+
+        if (pointer.x >= 600) {
+            jump(player_two);
+        }
+    })
 }
 
 function restarter() {
-    score = 0; 
+    score = 0;
     this.scene.restart();
 }
 
@@ -93,10 +107,10 @@ function genWall() {
     var hole = Math.floor(Math.random() * 5) + 1;
 
     // Add the 6 pipes with one two-part hole at position 'hole' and 'hole + 1'
-    for (var i = 0; i < 9; i++)
-        if (i != hole && i != hole + 1 && i != hole + 2) 
-            brickGen(800, i * 60 + 35);  
-    
+    for (var i = 0; i < 12; i++)
+        if (i != hole && i != hole + 1 && i != hole + 2)
+            brickGen(1200, i * 60 + 35);
+
     score += 1;
     scoreText.text = score;
 }
@@ -107,9 +121,9 @@ function jump(p) {
 }
 
 function update () {
-    if (player_one.y < 25 || player_one.y > 465) {
+    if (player_one.y < 25 || player_one.y > 700) {
         this.scene.restart();
-        score = 0; 
+        score = 0;
     }
 
     if (cursors.up.isDown)
@@ -117,7 +131,7 @@ function update () {
 
     if (p2_key.isDown)
         jump(player_one);
-    
+
     if (player_one.angle < 20) {
         player_one.angle += 1;
     }
